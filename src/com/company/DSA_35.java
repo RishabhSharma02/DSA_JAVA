93b8007e8ff32e4276ee834cd82a1d61ff5c4c6d
Rishabh Kumar Sharma <rishabhsharma28302@gmail.com>
2022-07-27 10:08:04 +0530

    Progs

--- src/com/company/DSA_35.java
+++ src/com/company/DSA_35.java
@@ -6,6 +6,7 @@ public class DSA_35 {
     public static void main(String[] args) {
         int[] arr={2,3,5,4,7,6};
         Arrays.sort(arr);
-        System.out.println(arr);
+        System.out.println(Arrays.toString(arr));
+
     }
 }
