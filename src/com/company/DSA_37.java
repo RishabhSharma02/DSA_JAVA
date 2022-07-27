a9bf92c7f4b601a38241a564baf9534d7465a989
Rishabh Kumar Sharma <rishabhsharma28302@gmail.com>
2022-07-27 10:43:11 +0530

    Progs

--- src/com/company/DSA_37.java
+++ src/com/company/DSA_37.java
@@ -1,5 +1,26 @@
 package com.company;
 
-public class DSA_37 {
+import java.util.Arrays;
 
+public class DSA_37 {
+    public static void main(String[] args) {
+        int[] arr={3,6,542,3,3556,234};
+        selection_sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    public static void selection_sort(int [] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int min=i;
+            int temp=arr[i];
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[j]<arr[min]) {
+                    min = j;
+                }
+            }
+        arr[i]=arr[min];
+            arr[min]=temp;
+
+        }
+    }
 }
